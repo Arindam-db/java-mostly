0ee8651a5680afb8d0d7f050147d50eae90a68c5
Arindam-db <debarindam225@gmail.com>
2024-07-06 22:00:24 +0530

    commit on 6th july

---
+++ HelloApp/Main.java
@@ -0,0 +1,17 @@
+package HelloApp;
+
+/**
+ * Main
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        int age = 78;
+        char gender = 'f';
+        boolean chad = false;
+        
+        System.out.println(+ age);
+        System.out.println(""+ gender);
+        System.out.println(""+ chad);
+    }
+}
