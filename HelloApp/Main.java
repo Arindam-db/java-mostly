f6cca196006e276143b5dbcee0240aa59abe8b5b
Arindam-db <debarindam225@gmail.com>
2024-07-06 22:11:50 +0530

    variables in java

--- HelloApp/Main.java
+++ HelloApp/Main.java
@@ -6,12 +6,17 @@ package HelloApp;
 public class Main {
 
     public static void main(String[] args) {
+
+        // Variables in java
+
         int age = 78;
         char gender = 'f';
         boolean chad = false;
+        double experience = 0.001;
         
         System.out.println(+ age);
         System.out.println(""+ gender);
         System.out.println(""+ chad);
+        System.out.println(""+ experience);
     }
 }
