6740d2abbfdaf39e878e731f6fedf3af3baf332f
Arindam-db <debarindam225@gmail.com>
2024-07-10 13:14:31 +0530

    10th july

---
+++ HelloApp/math01.java
@@ -0,0 +1,17 @@
+package HelloApp;
+
+
+public class math01 {
+    public static void main(String[] args) {
+        double x = 23.990;
+        double a = -26.878;
+        double y;
+
+        y = Math.min(x, a);
+        double n = Math.max(x, a);
+        double m = Math.abs(a);
+        System.out.println(y);
+        System.out.println(n);
+        System.out.println(m);
+    }
+}
