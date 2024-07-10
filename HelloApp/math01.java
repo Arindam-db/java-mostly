c1c48ec13cfaa5b79edd613b7ec0727d12af67b3
Arindam-db <debarindam225@gmail.com>
2024-07-10 20:00:47 +0530

    10th july evening

--- HelloApp/math01.java
+++ HelloApp/math01.java
@@ -5,13 +5,23 @@ public class math01 {
     public static void main(String[] args) {
         double x = 23.990;
         double a = -26.878;
-        double y;
+        double z = 89;
 
-        y = Math.min(x, a);
+        double y = Math.min(x, a);
         double n = Math.max(x, a);
         double m = Math.abs(a);
+        double p = Math.sqrt(z);
+        double k = Math.round(x);
+        double kk = Math.ceil(x);
+        double kp = Math.floor(x);
+
         System.out.println(y);
         System.out.println(n);
         System.out.println(m);
+        System.out.println(p);
+        System.out.println(k);
+        System.out.println(kk);
+        System.out.println(kp);
+
     }
 }
