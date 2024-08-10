05c43d7d9f3b4c4d8000964707a4f30994e3ecbd
Arindam-db <debarindam225@gmail.com>
2024-08-10 19:58:37 +0530

    backup

--- HelloApp/printf.java
+++ HelloApp/printf.java
@@ -4,9 +4,19 @@ public class printf {
     // printf = an optional method to control, format , and display text to the console window 
     //          two arguments = format string + object (object/variable/value)
     //          % [flags] [precision] [width] [conversion-character]
-public static void main(String[] args){
+    public static void main(String[] args){
+        boolean myboolean = false;
+        int p = 78;
+        double k = 23.67;
+        char mychar = '@';
+        String myString = "nrh";
 
-    System.out.printf("This is a format string %d",123);
+        // [conversion character]
+        System.out.printf("%b",myboolean);
+        System.out.printf("%d",p);
+        System.out.printf("%f",k);
+        System.out.printf("%c",mychar);
+        System.out.printf("%s",myString);
 
     }
 }
